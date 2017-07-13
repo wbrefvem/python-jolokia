6b9eb0fd4d04a7a3b31d1e42d8108e63d9619537
Will Refvem <wrefvem@redhat.com>
2017-07-12 23:56:29 -0400

    Decorators.........

--- jolokia/utils.py
+++ jolokia/utils.py
@@ -1,5 +1,10 @@
-from .exceptions import UrlNotSpecifiedException, MalformedUrlException
+from .exceptions import *
+from functools import wraps
 import re
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def validate_url(url):
@@ -9,3 +14,16 @@ def validate_url(url):
     regex = re.compile(r'^https?://([\w\d]+\.)*[\w\d]+(:[\d]{2,})*(/+[\w\d]*)*$')
     if not regex.match(url):
         raise MalformedUrlException('Base url should be of the form http[s]://hostname[:port][path]')
+
+
+def require_args(original_func, args, err_msg):
+
+    @wraps(original_func)
+    def new_func(self, *args, **kwargs):
+        try:
+            for arg in args:
+                kwargs[arg]
+            return original_func(self, *args, **kwargs)
+        except KeyError:
+            raise IllegalArgumentException(err_msg)
+    return new_func
