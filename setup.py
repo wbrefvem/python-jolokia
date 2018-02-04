28ec156bc9d6fb8ac1421e33ef6e8241143bbcef
Will Refvem <wrefvem@cloudbees.com>
2018-02-03 23:01:38 -0500

    Update to build configs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open(os.path.join(cwd, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='jolokia',
-    version='0.2.0',
+    version='0.2.1',
     description='A Python Jolokia client',
     long_description=long_description,
     url='https://github.com/wbrefvem/python-jolokia',
