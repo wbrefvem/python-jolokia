2b400b3386741566574d8db05db6b6f2d5ae5002
Grégoire Bellon-Gervais <greggbg@gmail.com>
2021-05-04 18:04:20 +0200

    Try 2 for pylint

--- jolokia/utils/decorators.py
+++ jolokia/utils/decorators.py
@@ -18,7 +18,7 @@ def require_params(reqs, err_msg):
                     argument = kwargs[arg]
                     LOGGER.debug(argument)
                 return func(*args, **kwargs)
-            except KeyError as e:
-                raise IllegalArgumentException(err_msg) from e
+            except KeyError as ke_except:
+                raise IllegalArgumentException(err_msg) from ke_except
         return _new_func
     return _wrapper
